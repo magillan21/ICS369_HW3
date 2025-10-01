72faecd33b1ef3db1aee8510b654f743317cdd10
Michaela <magillan@hawaii.edu>
2025-09-30 22:39:11 -1000

    Added new functions

--- ICS369_HW3_magillan.py
+++ ICS369_HW3_magillan.py
@@ -10,6 +10,10 @@ import random as rand
 # add two vectors
 def vecAdd(v1, v2):
     return (v1[0]+v2[0], v1[1]+v2[1], v1[2]+v2[2])
+
+# scale a vector
+def vecScale(v, s):
+    return (v[0]*s, v[1]*s, v[2]*s)
                 
 # create a cube in Maya
 def makeCube():
@@ -42,6 +46,13 @@ def updateNode(ptcl):
     pos = getPos(ptcl)
     cmds.move(pos[0], pos[1], pos[2], getNode(ptcl))
 
+# update particle velocity
+def applyForces(ptcl, gravity, wind):
+    vel = getVel(ptcl)
+    newVel = vecAdd(vel, gravity)
+    newVel = vecAdd(newVel, wind)
+    setVel(ptcl, newVel)
+
 # update particle position by its velocity
 def updatePos(ptcl):
     pos = getPos(ptcl)
@@ -49,9 +60,30 @@ def updatePos(ptcl):
     newPos = vecAdd(pos, vel)
     setPos(ptcl, newPos)
 
-# update the particle (position and node)
-def updateParticle(ptcl):
+# handle ground collision
+def handleCollision(ptcl, groundY, elasticity, friction):
+    pos = getPos(ptcl)
+    vel = getVel(ptcl)
+
+    # check if particle is at or below ground
+    if pos[1] <= groundY:
+        # move particle back to ground level
+        setPos(ptcl, pos[0], groundY, pos[2]))
+
+        # apply elasticity - reverse y velocity
+        newVelY = -vel[1] * elasticity
+
+        # apply friction - slow x and z components
+        newVelX = vel[0] * (1.0 - friction)
+        newVelZ = vel[2] * (1.0 - friction)
+
+        setVel(ptcl, (newVelX, newVelY, newVelZ))
+
+# update the particle (position. forces, collision, node)
+def updateParticle(ptcl, gravity, wind, groundY, elasticity, friction):
+    applyForces(ptcl, gravity, wind)
     updatePos(ptcl)
+    handleCollision(ptcl, groundY, elasticity, friction)
     updateNode(ptcl)
 
 # set keyframe for particle
@@ -59,26 +91,37 @@ def keyframeParticle(ptcl):
     cmds.setKeyframe(getNode(ptcl))
 
 # create and return a list of particles with random velocities
-def initParticles(num):
+def initParticles(num, minVel, maxVel):
     particleList = []
     for i in range(num):
         pos = (0, 10, 0)
-        vel = (rand.uniform(-1.0, 1.0), rand.uniform(-1.0, 1.0), rand.uniform(-1.0, 1.0))
+        vel = (rand.uniform(minVel[0], maxVel[0]), 
+               rand.uniform(minVel[1], maxVel[1]), 
+               rand.uniform(minVel[2], maxVel[2]))
         particleList.append(makeParticle(pos, vel))
     return particleList
 
 # run a list of particles for a number of frames
-def runParticles(particles, frames):
+def runParticles(particles, frames, gravity, wind, groundY, elasticity, friction):
     for frame in range(0, frames):
         cmds.currentTime(frame+1)
         for ptcl in particles:
-            updateParticle(ptcl)
+            updateParticle(ptcl, gravity, wind, groundY, elasticity, friction)
             keyframeParticle(ptcl)
 
 # test: initialize and run particles -------------------------------------------
+# set up parameters
+minVel = (-1.0, -1.0, -1.0)
+maxVel = (1.0, 1.0, 1.0)
+gravity = (0, -0.098, 0)  # gravity force in -y direction
+wind = (0.01, 0, 0)  # slight wind in +x direction
+groundY = 0  # ground plane at y=0
+elasticity = 0.7  # 70% bounce
+friction = 0.1  # 10% slowdown per collision
 
-particles = initParticles(100)
-runParticles(particles, 240)
+# create and run particles
+particles = initParticles(100, minVel, maxVel)
+runParticles(particles, 240, gravity, wind, groundY, elasticity, friction)
 
 
 
